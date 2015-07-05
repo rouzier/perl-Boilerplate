bff0f5c59bae079977d2451af6b2bc9c79ff0900
James Rouzier <rouzier@gmail.com>
2015-07-04 23:14:57 -0400

    Add methods get_data_map output_bundle process_file_path to create boilerplate

--- lib/Boilerplate/Bundle.pm
+++ lib/Boilerplate/Bundle.pm
@@ -2,6 +2,7 @@ package Boilerplate::Bundle;
 
 use warnings;
 use strict;
+use Template;
 
 =head1 NAME
 
@@ -14,6 +15,7 @@ Version 0.01
 =cut
 
 our $VERSION = '0.01';
+our %CACHED_DATA;
 
 =head1 SYNOPSIS
 
@@ -43,13 +45,59 @@ sub extract_data_section {
     my ($self) = @_;
     no strict 'refs';
     my $class = ref($self) || $self;
+    if(exists $CACHED_DATA{$class}) {
+        return $CACHED_DATA{$class};
+    }
     my $glob = *{"${class}::DATA"};
     local $/ = undef;
     my $data = <$glob>;
     $data = '' unless defined $data;
+    $CACHED_DATA{$class} = $data;
     return $data;
 }
 
+
+=head2 get_data_map
+
+=cut
+
+sub get_data_map {
+    my ($self) = @_;
+    my $data = $self->extract_data_section;
+    my @files = split /^=== *(.+?) *===r?\n/m, $data;
+    shift @files;
+    my %data = @files;
+    return \%data;
+}
+
+=head2 output_bundle
+
+TODO: documention
+
+=cut
+
+sub output_bundle {
+    my ($self, $vars) = @_;
+    my $map = $self->get_data_map;
+    my $tt = Template->new;
+    while(my ($file_path_template,$template) = each %$map) {
+        my $outfile = $self->process_file_path($file_path_template,$vars);
+        $tt->process(\$template, $vars, $outfile);
+    }
+}
+
+=head2 process_file_path
+
+TODO: documention
+
+=cut
+
+sub process_file_path {
+    my ($self, $file_path_template, $vars) = @_;
+    my $file_path = $file_path_template;
+    $file_path =~ s/\$([a-zA-Z0-9_]+)/$vars->{$1}/g;
+    return $file_path;
+}
 
 =head1 AUTHOR
 
